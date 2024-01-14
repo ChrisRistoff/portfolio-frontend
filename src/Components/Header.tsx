818f1a73f420dd8c40ac79a139b5d48e10e9655f
Krasyo <doanusho@gmail.com>
2024-01-14 14:29:27 +0000

    Add header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,16 @@
+import { Nav, Navbar } from "react-bootstrap";
+
+export const Header = () => {
+    return (
+        <Navbar bg="#333" variant="dark" expand="lg">
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav" className="justify-content-end">
+                <Nav>
+                    <Nav.Link href="#home" className="mx-2">Home</Nav.Link>
+                    <Nav.Link href="#portfolio" className="mx-2">Portfolio</Nav.Link>
+                    <Nav.Link href="#contact" className="mx-2">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
