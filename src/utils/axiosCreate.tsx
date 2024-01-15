0bbc02c44950c44ea6aa63d019820b25ddab45e9
Krasyo <doanusho@gmail.com>
2024-01-15 11:18:53 +0000

    Axios Change axios link to prepare for deployment

--- src/utils/axiosCreate.tsx
+++ src/utils/axiosCreate.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 // http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com:5000/api
 export const api = axios.create({
-  baseURL: 'http://localhost:5010/api',
+  baseURL: 'http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com:8080/api',
   timeout: 10000,
 });
