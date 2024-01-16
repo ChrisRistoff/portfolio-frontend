7103c2d04c1a894327aa298187f4588bf510bc40
Krasyo <doanusho@gmail.com>
2024-01-16 12:41:03 +0000

    Loading spinner on front page

--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { Row, Col, Image } from "react-bootstrap";
-import "../CSS/MainPage.css"; // Import your custom CSS file
+import {Row, Col, Image, Spinner} from "react-bootstrap";
+import "../CSS/MainPage.css";
 import { ContactForm } from "./ContactForm.tsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -9,12 +9,14 @@ import { getPersonalInfo } from "../utils/getPersonalInfo.tsx";
 export const Home = () => {
     const [animate, setAnimate] = useState(false);
     const [profileData, setProfileData] = useState<any>({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getProfileData = async () => {
             try {
                 let data = await getPersonalInfo();
                 setProfileData(data);
+                setLoading(false);
             } catch (error) {
                 console.error(error);
             }
@@ -28,40 +30,47 @@ export const Home = () => {
     }, []);
 
     return (
-        <div className="container">
-            <Row>
-                <Col md={6} className="profile-text">
-                    <h1 className={`animated-element ${animate ? "animate-in" : ""}`}>{profileData.name}</h1>
-                    <h3 className={`animated-element ${animate ? "animate-in" : ""}`}>{profileData.title}</h3>
-                    <p className={`animated-element ${animate ? "animate-in" : ""}`}>{profileData.bio}</p>
-                    <div className={`animated-element ${animate ? "animate-in" : ""}`}>
-                        <a href={profileData.github} target="_blank" rel="noopener noreferrer" className="contact-link">
-                            <FontAwesomeIcon icon={faGithub}/> Visit GitHub
-                        </a>
-                        {profileData.linkedin && (
-                            <a href={profileData.linkedin} target="_blank" rel="noopener noreferrer" className="contact-link">
-                                <FontAwesomeIcon icon={faLinkedin}/> Visit LinkedIn
+        loading ? (
+            <div className="loading-text">
+                <h3>Loading</h3>
+                <h1><Spinner animation="border" /></h1>
+            </div>
+        ) : (
+            <div className="container">
+                <Row>
+                    <Col md={6} className="profile-text">
+                        <h1 className={`animated-element ${animate ? "animate-in" : ""}`}>{profileData.name}</h1>
+                        <h3 className={`animated-element ${animate ? "animate-in" : ""}`}>{profileData.title}</h3>
+                        <p className={`animated-element ${animate ? "animate-in" : ""}`}>{profileData.bio}</p>
+                        <div className={`animated-element ${animate ? "animate-in" : ""}`}>
+                            <a href={profileData.github} target="_blank" rel="noopener noreferrer" className="contact-link">
+                                <FontAwesomeIcon icon={faGithub} /> Visit GitHub
                             </a>
-                        )}
+                            {profileData.linkedin && (
+                                <a href={profileData.linkedin} target="_blank" rel="noopener noreferrer" className="contact-link">
+                                    <FontAwesomeIcon icon={faLinkedin} /> Visit LinkedIn
+                                </a>
+                            )}
+                        </div>
+                    </Col>
+                    <Col md={6}>
+                        <Image
+                            src={profileData.image}
+                            alt={profileData.name}
+                            fluid
+                            className={`animated-element ${animate ? "animate-in" : ""} profile-image`}
+                        />
+                    </Col>
+                </Row>
+                <div className={`animated-element ${animate ? "animate-in" : ""}`}>
+                    <div className="contact-form-section">
+                        <ContactForm />
                     </div>
-                </Col>
-                <Col md={6}>
-                    <Image
-                        src={profileData.image}
-                        alt={profileData.name}
-                        fluid
-                        className={`animated-element ${animate ? "animate-in" : ""} profile-image`}
-                    />
-                </Col>
-            </Row>
-            <div className={`animated-element ${animate ? "animate-in" : ""}`}>
-            <div className="contact-form-section">
-                <ContactForm />
+                    <div className="projects-button">
+                        <a href="/projects" className="btn btn-outline-light">View My Projects</a>
+                    </div>
+                </div>
             </div>
-            <div className="projects-button">
-                <a href="/projects" className="btn btn-outline-light">View My Projects</a>
-            </div>
-        </div>
-        </div>
+        )
     );
 };
--- src/utils/axiosCreate.tsx
+++ src/utils/axiosCreate.tsx
@@ -1,7 +1,5 @@
-// axios create
 import axios from 'axios';
 
-// http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com:5000/api
 export const api = axios.create({
   baseURL: 'http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com:8080/api',
   timeout: 10000,
