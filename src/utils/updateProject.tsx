910d8b7133672a46ea7bf2990a5d0c7a2d4e8c55
Krasyo <doanusho@gmail.com>
2024-01-18 12:29:03 +0000

    Edit project Add authorization header to edit project axios util

--- src/utils/updateProject.tsx
+++ src/utils/updateProject.tsx
@@ -1,8 +1,13 @@
 import {api} from './axiosCreate.tsx';
+import {getTokenFromCookie} from "./login.tsx";
 
 export const updateProject = async (projectId: string, project: any) => {
   try {
-    const response = await api.patch(`/project-info/${projectId}`, project);
+    const response = await api.patch(`/project-info/${projectId}`, project, {
+      headers: {
+        'Authorization': `Bearer ${getTokenFromCookie()}`,
+      },
+    });
     return response.data;
   } catch (error) {
     console.error(error);
