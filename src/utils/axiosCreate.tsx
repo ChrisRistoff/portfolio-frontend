870f4ff6fb3319cb0b46414d2f3c62994bbe532b
Krasyo <doanusho@gmail.com>
2024-01-23 08:41:48 +0000

    change api link

--- src/utils/axiosCreate.tsx
+++ src/utils/axiosCreate.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const local: string = "http://localhost:5010/api"
-const live: string = "http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com:8080/api"
+// http://ec2-35-179-90-244.eu-west-2.compute.amazonaws.com:8080/api
+const live: string = "https://krasen.site/apis"
 export const api = axios.create({
   baseURL: live,
   timeout: 10000,
