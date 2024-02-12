00a565036f3d41323169c04557aaf829ea363fb3
Krasyo <doanusho@gmail.com>
2024-02-12 20:43:10 +0000

    Center nav links

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -18,7 +18,7 @@ export const Header = () => {
         <div className={`animated-element ${animate ? "animate-in" : ""}`}>
         <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
             <Navbar.Toggle aria-controls="navbar-nav" />
-            <Navbar.Collapse id="navbar-nav" className="justify-content-end">
+            <Navbar.Collapse id="navbar-nav" className="justify-content-center">
                 <Nav>
                     <Nav.Link href="/" className="nav-link-custom mx-2">Home</Nav.Link>
                     <Nav.Link href="/projects" className="nav-link-custom mx-2">Projects</Nav.Link>
